d4f36d93985af2bbdf74fec49be358f27d5da84c
shishamoid <itodaisuke0812@gmail.com>
2022-04-14 18:12:33 +0900

    refactor and change circle_train_data

--- kadai2/2.py
+++ kadai2/2.py
@@ -1,12 +1,11 @@
-torch.cuda.is_available(﻿)
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-import torchvision
-from torchvision import transforms
-from torchvision.datasets import MNIST # CIFAR10もインポートしておく
-import numpy as np
 import matplotlib.pyplot as plt
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import torchvision
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+import os
 
 
 class Encoder(torch.nn.Module):
@@ -16,12 +15,14 @@ class Encoder(torch.nn.Module):
         self.fc2 = torch.nn.Linear(512, 64)
         self.fc3 = torch.nn.Linear(64, 16)
         self.fc4 = torch.nn.Linear(16, 2)
+
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+
 
 class Decoder(torch.nn.Module):
     def __init__(self, output_size):
@@ -30,43 +31,35 @@ class Decoder(torch.nn.Module):
         self.fc2 = torch.nn.Linear(16, 64)
         self.fc3 = torch.nn.Linear(64, 512)
         self.fc4 = torch.nn.Linear(512, output_size)
+
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = torch.tanh(self.fc4(x))  # -1～1に変換
+        x = torch.tanh(self.fc4(x))
         return x
+
 
 class AutoEncoder(torch.nn.Module):
     def __init__(self, org_size):
         super().__init__()
         self.enc = Encoder(org_size)
         self.dec = Decoder(org_size)
+
     def forward(self, x):
-        x = self.enc(x)  # エンコード
-        x = self.dec(x)  # デコード
+        x = self.enc(x)
+        x = self.dec(x)
         return x
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-trainset = MNIST('./data', train=True, transform=transform, download=True)
-testset = MNIST('./data', train=False, transform=transform, download=True)
-
-batch_size = 50
-trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 def imshow(img):
     img = torchvision.utils.make_grid(img)
     img = img / 2 + 0.5
     npimg = img.detach().numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    return npimg
 
-iterator = iter(trainloader)
-x, _ = next(iterator)
-imshow(x)
 
-def train(net, criterion, optimizer, epochs, trainloader):
+def train(net, criterion, optimizer, epochs, trainloader, input_size):
     losses = []
     output_and_label = []
 
@@ -86,16 +79,46 @@ def train(net, criterion, optimizer, epochs, trainloader):
         print('loss:', avg_loss)
         output_and_label.append((output, img))
     print('finished')
+
     return output_and_label, losses
 
-input_size = 28 * 28
-net = AutoEncoder(input_size)
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
-EPOCHS = 100
 
-output_and_label, losses = train(net, criterion, optimizer, EPOCHS, trainloader)
+def resultshow(output_and_label, losses):
 
-output, org = output_and_label[-1]
-imshow(org.reshape(-1, 1, 28, 28))
-imshow(output.reshape(-1, 1, 28, 28))
+    output, org = output_and_label[-1]
+    npimg = imshow(org.reshape(-1, 1, 28, 28))
+    plt.imsave("./autoencoder_pictures/original_picture.png",
+               np.transpose(npimg, (1, 2, 0)))
+
+    npimg = imshow(output.reshape(-1, 1, 28, 28))
+    plt.imsave("./autoencoder_pictures/output_picture.png",
+               np.transpose(npimg, (1, 2, 0)))
+
+    plt.imsave("./autoencoder_pictures/loss.png",
+               losses)
+
+
+def main():
+    os.makedirs("./autoencoder_pictures", exist_ok=True)
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    trainset = MNIST('./data', train=True, transform=transform, download=True)
+
+    batch_size = 50
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+    input_size = 28 * 28
+    net = AutoEncoder(input_size)
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
+    EPOCHS = 100
+
+    output_and_label, losses = train(
+        net, criterion, optimizer, EPOCHS, trainloader, input_size)
+
+    resultshow(output_and_label, losses)
+
+
+if __name__ == '__main__':
+    main()
